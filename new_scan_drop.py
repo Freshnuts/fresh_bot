31b2627e960fc558b505027b0e5cc8b91439de8a
root <fr3shnuts@gmail.com>
2019-06-13 23:12:35 -0700

    updated, added 2 VMs. cleaned authentication errors.

--- new_scan_drop.py
+++ new_scan_drop.py
@@ -9,20 +9,21 @@ num = 0
 num2 = 0
 
 def ssh_start():
-    """worker function"""
     ip = "192.168.203.%d" % i
     print "Connecting:", ip, user_list[num] + pass_list[num2]
     p = paramiko.SSHClient()
     p.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    p.connect(ip, username=user_list[num], password=pass_list[num2])
-    stdin, stdout, stderr = p.exec_command('ls')
+    try:
+        p.connect(ip, username=user_list[num], password=pass_list[num2])
+        stdin, stdout, stderr = p.exec_command('ls')
+        for line in stdout:
+            print(line.strip('\n'))
+        for line in stderr:
+            print(line.strip('\n'))
+    except paramiko.AuthenticationException:
+        print "Connection failed: " + ip  
 
-    for line in stdout:
-        print(line.strip('\n'))
-    for line in stderr:
-        print(line.strip('\n'))
-
-for i in range(150,152):
+for i in range(150,155):
     for num in range(0,4):
         for num2 in range(0,4):
             p = multiprocessing.Process(target=ssh_start)
